42e971f04a5b622246b3088081fc1c779bc17062
johnaxlerose <johnaxleroseditablan@gmail.com>
2022-02-19 04:20:49 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+$(".faq-toggle-title-first-ad").on("click", function (){
+  $(".faq-toggle-content1-first-ad").slideToggle('active');
+});
+$(".faq-toggle-title-second-ad").on("click", function (){
+    $(".faq-toggle-content2-second-ad").slideToggle('active');
+  });
+  $(".faq-toggle-title-third-ad").on("click", function (){
+    $(".faq-toggle-content3-third-ad").slideToggle('active');
+  });
+  $(".faq-toggle-title-fourth-ad").on("click", function (){
+    $(".faq-toggle-content4-fourth-ad").slideToggle('active');
+  });
+  $(".faq-toggle-title-fifth-ad").on("click", function (){
+    $(".faq-toggle-content5-fifth-ad").slideToggle('active');
+  });
+
+
+
+/*
+$(document).ready(function(){
+$("#faq1").click(function(){
+$("faq-toggle-content1").slideDown(300).toggleClass("active");
+});
+});
+*/
+
